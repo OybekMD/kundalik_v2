d654b47e53bee09bebb3556b562cf9f8b304d3cd
oybekmd <oybekatamatov999@gmail.com>
2023-07-06 11:41:00 +0500

    modified:   config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = 'django-insecure-2ptpxm_m7ng!gb9aerp*s6*h0a1fqme##nd(z$^+o!^o-+cyv-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
